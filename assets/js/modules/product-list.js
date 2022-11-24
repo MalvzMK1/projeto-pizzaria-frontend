53b93f50d57c8062060a07cb28bf1f681ce72993
Matheus Alves <matheus.adeoliveira11@gmail.com>
2022-11-24 18:27:09 -0300

    go back home button

--- assets/js/modules/product-list.js
+++ assets/js/modules/product-list.js
@@ -2,15 +2,13 @@ export default function listenProductTypes () {
   let productListElements = document.querySelectorAll('.product-type');
   
   productListElements.forEach(element => {
-    if (element.classList.contains('selected'))
-      element.classList.remove('selected');
-
     element.addEventListener('click', () => {
       if (!element.classList.contains('selected')) {
         productListElements.forEach(element => {
           element.classList.contains('selected') ? element.classList.remove('selected') : false
         });
         element.classList.add('selected');
+        document.querySelector('.product-type-title').textContent = element.textContent;
       }
       else if (element.classList.contains('selected'))
         element.classList.remove('selected');

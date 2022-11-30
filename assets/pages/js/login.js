a9e7dc0462bf743cb2834d81276ba6e4f7df5081
Matheus Alves <matheus.adeoliveira11@gmail.com>
2022-11-30 11:28:16 -0300

    cms login finished and home in progress

---
+++ assets/pages/js/login.js
@@ -0,0 +1,19 @@
+const emailInput = document.querySelector('#admin-email-input');
+const passwordInput = document.querySelector('#admin-password-input');
+const submitLoginFormButton = document.querySelector(
+  '#admin-login-submit-button'
+);
+
+const validateInputs = () => {
+  if (emailInput.value === '' || passwordInput.value === '') return false;
+  return true;
+};
+
+submitLoginFormButton.addEventListener('click', () => {
+  if (validateInputs()) location.href = './cms-home.html';
+  else {
+    emailInput.style.borderBottom = '3px solid var(--red-500)';
+    passwordInput.style.borderBottom = '3px solid var(--red-500)';
+    submitLoginFormButton.style.animation = 'shake 500ms ease-in-out';
+  }
+});

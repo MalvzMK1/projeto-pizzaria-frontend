16f4e589326bce6e65ec7efd36e2fd5d2054bf13
Matheus Alves <matheus.adeoliveira11@gmail.com>
2022-12-14 20:28:17 -0300

    changes

--- assets/js/modules/product-list.js
+++ assets/js/modules/product-list.js
@@ -7,7 +7,6 @@ export default function listenProductTypes() {
   productListElements.forEach((element) => {
     element.addEventListener('click', (el) => {
       renderProducts();
-      changeProductSizes(el.target);
       if (!element.classList.contains('selected')) {
         productListElements.forEach((element) => {
           element.classList.contains('selected')
@@ -22,41 +21,3 @@ export default function listenProductTypes() {
     });
   });
 }
-
-const changeProductSizes = async (target) => {
-  const sizeSelect = document.querySelector('#size-filter');
-  const sizes = new Array();
-
-  sanitizeSelectOptions();
-  if (target.textContent.toLowerCase().includes('pizza')) {
-    const { pizzas } = await getPizzas();
-    pizzas.forEach((item) => {
-      if (!sizes.includes(item.tamanho)) sizes.push(item.tamanho);
-    });
-  } else if (target.textContent.toLowerCase().includes('bebida')) {
-    const { bebidas } = await getDrinks();
-    bebidas.forEach((item) => {
-      if (!sizes.includes(item.tamanho)) sizes.push(item.tamanho);
-    });
-    // drinkSizes.forEach((size) => {
-    //   const sizeOption = document.createElement('option');
-    //   sizeOption.value = size;
-    //   sizeOption.classList.add('size-option');
-    //   sizeOption.textContent = size;
-    //   sizeSelect.appendChild(sizeOption);
-    // });
-  }
-  sizes.forEach((item) => {
-    const sizeOption = document.createElement('option');
-    sizeOption.value = item;
-    sizeOption.classList.add('size-option');
-    sizeOption.textContent = item;
-    sizeSelect.appendChild(sizeOption);
-  });
-};
-
-const sanitizeSelectOptions = () => {
-  document.querySelectorAll('.size-option').forEach((option) => {
-    option.remove();
-  });
-};

1c0a3ce8016b430e573acd460dd1dfd0024c34b5
Matheus Alves <matheus.adeoliveira11@gmail.com>
2022-12-14 00:29:53 -0300

    registering a new user

--- admin/js/newAdmin.js
+++ admin/js/newAdmin.js
@@ -1,9 +1,67 @@
-import { getImage } from './utils/imageReader.js';
+// import { getImage } from './utils/imageReader.js';
 
-const adminNameInput = document.querySelector('#new-admin-name');
-const adminEmailInput = document.querySelector('#new-admin-email');
-const adminPasswordInput = document.querySelector('#new-admin-password');
-const adminConfirmPasswordInput = document.querySelector('#new-admin-password');
-const adminPhotoInput = document.querySelector('#new-admin-photo');
+import { createAdmin } from './utils/integrations/adminAxios.js';
 
-getImage(adminPhotoInput);
+// getImage(adminPhotoInput);
+
+const validateInputs = () => {
+  const adminName = document.querySelector('#new-admin-name').value;
+  const adminEmail = document.querySelector('#new-admin-email').value;
+  const adminPassword = document.querySelector('#new-admin-password').value;
+  const adminConfirmPassword = document.querySelector(
+    '#confirm-admin-password'
+  ).value;
+
+  if (
+    adminName === '' ||
+    adminEmail === '' ||
+    adminPassword === '' ||
+    adminConfirmPassword === ''
+  )
+    return false;
+  return true;
+};
+
+const validatePassword = (password, confirmPassword) => {
+  if (password === confirmPassword) return true;
+  return false;
+};
+
+const getFormValues = () => {
+  const adminName = document.querySelector('#new-admin-name').value;
+  const adminEmail = document.querySelector('#new-admin-email').value;
+  const adminPassword = document.querySelector('#new-admin-password').value;
+  const adminConfirmPassword = document.querySelector(
+    '#confirm-admin-password'
+  ).value;
+  // const adminPhotoInput = document.querySelector('#new-admin-photo');
+  const validatedPassword = validatePassword(
+    adminPassword,
+    adminConfirmPassword
+  );
+
+  if (validatedPassword) {
+    const adminJSON = {
+      name: adminName,
+      email: adminEmail,
+      password: adminPassword,
+      photo:
+        'https://tm.ibxk.com.br/2021/04/23/23165043458380.jpg?ims=1200x675',
+    };
+
+    return adminJSON;
+  }
+  alert('As senhas não batem');
+  return false;
+};
+
+document
+  .querySelector('.submit-button')
+  .addEventListener('click', async (el) => {
+    if (validateInputs()) {
+      const admin = getFormValues();
+      if (admin) await createAdmin(admin);
+    }
+    else 
+      alert('Preencha os campos');
+  });
